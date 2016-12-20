8549a99264c186f969e8a5d38238b6d27a382de8
Jim Kennedy <jim8786453@gmail.com>
2016-12-20 10:34:58 +0000

    Fix bug in test

--- sync/tests/test_sync.py
+++ sync/tests/test_sync.py
@@ -967,7 +967,7 @@ class TestSync():
 
     def test_storage_get_many_empty(self):
         nodes = sync.Node.get()
-        assert nodes is []
+        assert nodes == []
 
 
 class TestBaseStorage():
