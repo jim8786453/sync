b6f0e03605c16ba6b42695a3844e279f371749fb
Jim Kennedy <jim8786453@gmail.com>
2018-11-06 19:47:56 +0000

    Upgrade dev env
    - Mongo 4.0
    - Python 3.x

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import os
 import platform
 
 from setuptools import setup
-from pip.req import parse_requirements
 
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 req_file = 'requirements.txt'
-if platform.python_implementation() != 'CPython':
-    req_file = 'requirementsPyPy.txt'
 install_reqs = parse_requirements(req_file, session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 del os.link
